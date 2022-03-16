6861fd5e6646de8febfdab3869ff415a65d27ba4
inees10 <101189124+inees10@users.noreply.github.com>
2022-03-16 15:17:16 +0100

    Add files via upload

---
+++ Pagina inicio/inicio_main.js
@@ -0,0 +1,10 @@
+function nombre(){
+    var nombre = document.getElementById('nombre').value;
+
+    document.formulario.nombreobtenido.value= nombre;
+}
+/*function nombre(usuario){
+    document.getElementById("nombre").innerHTML = usuario.nombre;
+}*/
+
+//document.getElementById("nombre").innerHTML = usuario.nombre;
