1f4d8441eefcf0752dc00ee40acafc35185aa4d3
magajimenez10 <100429306@alumnos.uc3m.es>
2022-04-03 18:50:52 +0200

    Movimiento de osbtaculos

--- amm22-pweb-gst-mavi_team-main/Pagina juego/Juego/main.js
+++ amm22-pweb-gst-mavi_team-main/Pagina juego/Juego/main.js
@@ -11,6 +11,7 @@ document.getElementById("nombre").innerHTML = getNombre('username');
 
 var canvas = document.getElementById('campo');
 var ctx = canvas.getContext('2d');
+var frameNo = 0;
 var nivel = 1;
 
 //Objetos//
@@ -29,16 +30,47 @@ var moneda = {
     img: new Image()
 };
 
+// var obsAbuela = {
+//     obsAbuelax: campo.width - 2*zona - borde,
+//     obsAbuelay: 150,
+//     img: new Image,
+//     // update: function(){
+//     //     ctx = canvas.context;
+//     //     ctx.fillStyle = color;
+//     //     ctx.fillRect(this.x, this.y, this.width, this.height);
+//     // },
+//     // newPos : function() {
+//     //     this.x += this.speedX;
+//     //     this.y += this.speedY;        
+//     // }
+// };
+
+var posJugadorX, posJugadorY = 0;
+var obsX, obsY;
+var obsAbuela = new Image;
+var obstaculos = [];
 
 //Main//
 
 function main(){
+
+    obsX = canvas.height / 2;
+    obsY = canvas.width;
+
+}
+
+function dibujar() {
     clear();
 
     //Dibujar
     dibujarJ();
     dibujarM();
-    dibujarO();
+
+    if (obstaculos.length != 0) {
+        dibujarO();
+        i = 0;
+    }
+
     dibujarZ();
     dibujarP();
     dibujarT();
@@ -60,16 +92,31 @@ function main(){
     }
 
     //Movimiento de los obstáculos
+    //updateGameArea();
+    // for (i = 0; i < obsAbuela; i += 1) {
+    //     myObstacles.x += -1;
+    //     myObstacles[i].update();
+    // }
+    // obsAbuela.x += -0.5;
+    
+
+    //Este for añade los obstaculos cada tanto
+
 
     //Colisiones con bordes
 
     //Colisiones con obstáculos
 
-    setInterval(main, 10);
     
 }
 
 //Funciones//
+
+function obst (posJugadorX, posJugadorY) {
+    //Funcion para crear los obstáculos
+    this.obsX = obsX;
+    this.obsY = obsY;
+}
 
 //Dibujar
 
@@ -85,7 +132,24 @@ function dibujarM(){
 }
 
 function dibujarO(){
-    //Maga
+    //Funcion para dibujar los obstáculos
+    for(var i = 0; i < obstaculos.length; i++) {
+        ctx.drawImage(obsAbuela, obstaculos[i].obsX, obstaculos[i].obsY, ancho, ancho);
+        //3 niveles, 3 velocidades distintas? con case o if se hace 
+        obstaculos[i].obsX -= 3;
+        if(obstaculos[i].obsX < 0) {
+            obstaculos.splice(i,1);
+        }
+    }
+}
+
+function creaObstaculo (){
+    //Funcion para generar los obstaculos, como si fuesen los objetos
+    var obstA = new obst (obsX, obsY);
+    obsAbuela.src = 'imagen/abuela1.png';
+    obstA.obsX = campo.width;
+    obstA.obsY = Math.floor(Math.random() * (campo.height - obsAbuela.height));
+    obstaculos.push(obstA);
 }
 
 function dibujarZ(){
@@ -121,6 +185,10 @@ function nuevaM(){
 
 function abrirP(){
     //Borrar tornos
+}
+
+function movObstaculos () {
+
 }
 
 //Nombre del formulario
@@ -171,11 +239,5 @@ function clear(){
     ctx.clearRect(0, 0, campo.width, campo.height);
 }
 
-
-
-
-
-
-
-
-
+setInterval(dibujar, 10);
+setInterval(creaObstaculo, 1000);
